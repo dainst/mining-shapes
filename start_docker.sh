dc67197bfa95e6dc3263b97a36136436e6f53f3b
Michael Kihm <michael.kihm@web.de>
2020-05-05 15:04:25 +0200

    Typo correction

--- start_docker.sh
+++ start_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 if type nvidia-smi > /dev/null || wmic path win32_VideoController get name | grep -q "NVIDIA"; then
- var="Docker_CPU"
+ var="Docker_GPU"
 else
  var="Docker_CPU"
 fi
