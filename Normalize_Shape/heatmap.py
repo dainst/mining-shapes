463baec9e9acf2f865462e7cb305553541c55ad9
Michael Kihm <michael.kihm@web.de>
2020-06-04 12:36:05 +0200

    Added scaling - scales keypoint to size of input images

--- Normalize_Shape/heatmap.py
+++ Normalize_Shape/heatmap.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2 as cv
 from typing import Tuple
 
 def gaussian_k(x0,y0,sigma, width, height):
@@ -33,17 +34,21 @@ def render_heatmap(heatmap:np.ndarray):
     """
     return np.sum(heatmap,axis=2)
 
-def render_heatmap_in_image(image:np.ndarray,heatmap:np.ndarray, color:Tuple=(0,255,0)):
+def render_heatmap_in_image(image:np.ndarray,heatmap:np.ndarray, hm_scale:Tuple=(4,4), color:Tuple=(0,255,0)):
     """
     @brief Merge source image with heatmap in one images
     @param image 3 channel image
     @param heatmap heatmap array
     @color color in which the heatmaps will be rendered
+    @hm_scale scaling factor of heatmap image
     """
-    assert image.shape[0] == heatmap.shape[0] and image.shape[1] == heatmap.shape[1],\
+    h_scale, w_scale = hm_scale
+    hm_height, hm_width = heatmap.shape[:2]
+    _heatmap = cv.resize(heatmap,(w_scale*hm_width,h_scale*hm_height))
+    assert image.shape[0] == _heatmap.shape[0] and image.shape[1] == _heatmap.shape[1],\
             "Shape from image and heatmap dont match"
-    points = render_heatmap(heatmap)
+    points = render_heatmap(_heatmap)
     img = np.zeros_like(image)
-    img[np.where(points >0.4)] = color
+    img[np.where(points >0.8)] = color
 
     return (image*0.2 + img *0.8)/255
