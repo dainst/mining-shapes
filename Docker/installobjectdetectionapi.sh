df8b248cc1de4cdfc3eeb29fff6a5b3c2ce2f758
Michael Kihm <michael.kihm@web.de>
2020-10-12 10:32:01 +0200

    Change Dockerfile architecture - Everything put in one Directory - Dockerfiles tensorflow/tensorflow:1.15.3 / tensorflow/tensorflow:1.15.3-gpu - Added installations scripts to be used for GPU and CPU docker - Distinguish between .dev and normal Dockerfile and docker-compose - Updated object detection-api installation

---
+++ Docker/installobjectdetectionapi.sh
@@ -0,0 +1,30 @@
+git clone https://github.com/tensorflow/models.git
+
+# Install pycocoapi
+git clone --depth 1 https://github.com/cocodataset/cocoapi.git 
+cd cocoapi/PythonAPI 
+make -j8
+cp -r pycocotools /models/research 
+cd ../../
+rm -rf cocoapi
+
+# Get protoc 3.0.0, rather than the old version already in the container
+curl -OL "https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip" 
+unzip protoc-3.0.0-linux-x86_64.zip -d proto3
+mv proto3/bin/* /usr/local/bin
+mv proto3/include/* /usr/local/include
+rm -rf proto3 protoc-3.0.0-linux-x86_64.zip
+
+# Run protoc on the object detection repo
+cd models/research
+protoc object_detection/protos/*.proto --python_out=.
+# Install TensorFlow Object Detection API.
+cp object_detection/packages/tf1/setup.py .
+python -m pip install --use-feature=2020-resolver .
+
+# Set the PYTHONPATH to finish installing the API
+PYTHONPATH=$PYTHONPATH:/models/research/slim
+PYTHONPATH=$PYTHONPATH:/models/research
+
+#Run object detection API unit tests
+python object_detection/builders/model_builder_tf1_test.py
---
+++ Docker/installscript.sh
@@ -0,0 +1,11 @@
+# install build utilities
+apt-get update && apt-get install -y gcc make apt-transport-https ca-certificates build-essential
+
+apt-get install -y git
+apt-get update && apt-get install -y libgl1-mesa-dev
+apt-get install -y graphviz
+apt-get install -y tesseract-ocr
+apt-get install -y curl unzip
+
+# Install object detection api dependencies
+DEBIAN_FRONTEND=noninteractive apt-get install -y protobuf-compiler python3-pil python3-lxml python3-tk
