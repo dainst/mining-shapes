3a775f4f93d781a83888c65d900608de900327b7
maxhaibt <55451612+maxhaibt@users.noreply.github.com>
2020-10-14 18:07:43 +0200

    Update installscript.sh
    popler utils

--- Docker/installscript.sh
+++ Docker/installscript.sh
@@ -6,6 +6,7 @@ apt-get update && apt-get install -y libgl1-mesa-dev
 apt-get install -y graphviz
 apt-get install -y tesseract-ocr
 apt-get install -y curl unzip
+apt-get update && apt-get install -y poppler-utils
 
 # Install object detection api dependencies
 DEBIAN_FRONTEND=noninteractive apt-get install -y protobuf-compiler python3-pil python3-lxml python3-tk
