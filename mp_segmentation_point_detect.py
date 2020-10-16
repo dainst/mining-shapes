98bb4d358ae3dcbc2cadd7bda722edb88a8814a5
Michael Kihm <michael.kihm@web.de>
2020-10-16 09:48:23 +0200

    Changed to command line interface

--- mp_segmentation_point_detect.py
+++ mp_segmentation_point_detect.py
@@ -1,25 +1,45 @@
 
 import os
+import argparse
 from mining_pages_utils.tensorflow_utils import run_vesselprofile_segmentation, run_point_detection
 
 
-SEG_MODEL = '/home/Code/Shape_Segmentation/training_resnet34_512_512_epochs_100_2020-10-09.h5'
-#POINT_MODEL = '/home/images/OUTPUT/Point_Detector_trained_80_2020-10-12.h5'
-POINT_MODEL = '/home/Code/Normalize_Shape/point_network_trained_80_256x256_20200723_augdata.h5'
+def process_vesselprofiles(_args):
 
-OUTPATH = '/home/images/OUTPUT/'
-VESSELLPATH = OUTPATH + 'vesselprofiles/'
-SEGMENTPATH = OUTPATH + 'segmented_profiles/'
-POINTPATH = OUTPATH + 'points'
+    OUTPATH = _args.outpath
+    SEGMENTPATH = os.path.join(OUTPATH, 'segmented_profiles/')
+    POINTPATH = os.path.join(OUTPATH, 'points/')
+    VESSELLPATH = _args.vesselpath
 
-for path in [POINTPATH, SEGMENTPATH]:
-    if not os.path.exists(path):
-        os.makedirs(path)
+    SEG_MODEL = _args.segment_model
+    POINT_MODEL = _args.point_model
 
-# Profile segmentation
-print('Perform image segmentation')
-run_vesselprofile_segmentation(VESSELLPATH, SEGMENTPATH, SEG_MODEL)
+    # Create output dirs
+    for path in [POINTPATH, SEGMENTPATH]:
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-# Point detection
-print('Perform point detection')
-run_point_detection(VESSELLPATH, POINTPATH, POINT_MODEL)
+    # Profile segmentation
+    print('Perform image segmentation')
+    run_vesselprofile_segmentation(VESSELLPATH, SEGMENTPATH, SEG_MODEL)
+
+    # Point detection
+    print('Perform point detection')
+    run_point_detection(VESSELLPATH, POINTPATH, POINT_MODEL)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run profile segmentation and perform point detection."
+    )
+    parser.add_argument("--vesselpath", type=str,
+                        help="Path in which vesselprofiles are located", required=True)
+    parser.add_argument("--outpath", type=str,
+                        help="Output path to save processed vesselprofiles", required=True)
+    parser.add_argument("--segment_model", type=str,
+                        help="Path of segmetation model (should be a Keras .h5 file)", required=True)
+    parser.add_argument("--point_model", type=str,
+                        help="Path of point detection model (should be a Keras .h5 file)", required=True)
+
+    args = parser.parse_args()
+    process_vesselprofiles(args)
