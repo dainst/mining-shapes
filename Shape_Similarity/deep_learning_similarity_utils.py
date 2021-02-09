484338383fb530e896d33d583145e31149cf06df
Michael <michael.kihm@web.de>
2021-02-09 23:25:54 +0100

    removed input shape for ResNet features

--- Shape_Similarity/deep_learning_similarity_utils.py
+++ Shape_Similarity/deep_learning_similarity_utils.py
@@ -21,13 +21,11 @@ class ResnetFeatureVectors:
     @brief: Data generator to return feature vector of given keras model
     @param images_path: location of source images
     @param model: path to keras Model
-    @param image: size to rescale images to
     """
 
-    def __init__(self, images_path: str, model: keras.Model, image_size) -> None:
+    def __init__(self, images_path: str, model: keras.Model) -> None:
         self._images_path = images_path
         self._model = model
-        self._image_size = image_size
 
     def _resourceId_from_file(self, filename: str) -> str:
         return Path(filename).stem
@@ -40,7 +38,7 @@ class ResnetFeatureVectors:
 
     def __iter__(self) -> FeatureEntry:
         for img_path in glob.glob(f"{self._images_path}/*.jpg"):
-            img = cv.resize(self._read_img_rgb(img_path), self._image_size)
+            img = self._read_img_rgb(img_path)
             resource_id = self._resourceId_from_file(img_path)
             feature_vec = self._model.predict(
                 img[np.newaxis, ...], verbose=False)
@@ -51,8 +49,7 @@ def featurevector_to_db(
         path: str,
         db_url: str,
         db_name: str,
-        auth: Tuple[str, str],
-        input_shape: Tuple[int, int, int] = (512, 512, 3)) -> None:
+        auth: Tuple[str, str]) -> None:
     """
     @brief: Read images in path, compute resnet feature vectors and PUT data into RUNNING PouchDB instance
             of idai-field
@@ -60,14 +57,13 @@ def featurevector_to_db(
     @param db_url: url of running pouchDB instance
     @db_name: name of database
     @param auth: authentication for PouchDB (username, password)
-    @param input_shape shape to scale images
     """
     pouchDB_url = f'{db_url}/{db_name}'
 
     model = keras.applications.resnet50.ResNet50(
-        input_shape=input_shape, include_top=False, pooling='avg', weights='imagenet')
+        include_top=False, pooling='avg', weights='imagenet')
 
-    vector_generator = ResnetFeatureVectors(path, model, input_shape[:2])
+    vector_generator = ResnetFeatureVectors(path, model)
     with tqdm(total=len(vector_generator)) as pbar:
         for feature in vector_generator:
             put_data_in_pouchdb(pouchDB_url,
