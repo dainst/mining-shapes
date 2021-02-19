34e0ce80f210045c8d115ddae41330c227028ebe
Michael <michael.kihm@web.de>
2021-02-19 15:58:19 +0100

    added image size again to ResNetFeatures

--- Shape_Similarity/ResNetFeatureVectors/resnetfeaturevectors.py
+++ Shape_Similarity/ResNetFeatureVectors/resnetfeaturevectors.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import glob
 from pathlib import Path
 from tensorflow import keras
+from typing import Tuple
 # pylint: disable=relative-beyond-top-level
 from ..db_utils.featureentry import FeatureEntry
 
@@ -12,11 +13,13 @@ class ResnetFeatureVectors:
     @brief: Data generator to return feature vector of given keras model
     @param images_path: location of source images
     @param model: path to keras Model
+    @param image: size to rescale images to
     """
 
-    def __init__(self, images_path: str, model: keras.Model) -> None:
+    def __init__(self, images_path: str, model: keras.Model, image_size: Tuple[int, int] = (512, 512)) -> None:
         self._images_path = images_path
         self._model = model
+        self._image_size = image_size
 
     def _resourceId_from_file(self, filename: str) -> str:
         return Path(filename).stem
@@ -29,7 +32,7 @@ class ResnetFeatureVectors:
 
     def __iter__(self) -> FeatureEntry:
         for img_path in glob.glob(f"{self._images_path}/*.jpg"):
-            img = self._read_img_rgb(img_path)
+            img = cv.resize(self._read_img_rgb(img_path), self._image_size)
             resource_id = self._resourceId_from_file(img_path)
             feature_vec = self._model.predict(
                 img[np.newaxis, ...], verbose=False)
