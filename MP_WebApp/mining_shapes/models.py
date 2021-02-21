f5a8676f8839a4ffcce6b3459e52b24553acd276
Michael <michael.kihm@web.de>
2021-02-21 15:04:25 +0100

    Added VesselProfile and Session models

--- MP_WebApp/mining_shapes/models.py
+++ MP_WebApp/mining_shapes/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+from django.db.models.fields import CharField
+from django.db.models.fields.files import ImageField
+
+from user.models import User
+
+
+class Session(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class VesselProfile(models.Model):
+    filename = models.CharField(max_length=255)
+    input_image = ImageField(upload_to="orig_image")
+    segmented_image = models.ImageField(upload_to="seg_images")
+    catalog = models.CharField(max_length=255)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    # vector field
+
+    def __str__(self) -> str:
+        return f"{self.filename}_{self.catalog}"
