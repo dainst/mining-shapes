9003858879e5d6e043182975f45c3ca5c57498a4
Michael <michael.kihm@web.de>
2021-02-22 10:13:51 +0100

    added process route

--- MP_WebApp/mining_shapes/urls.py
+++ MP_WebApp/mining_shapes/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('process/<int:session_id>/<str:seg_model>/<str:features>',
+         views.process, name='process'),
 ]
--- MP_WebApp/mining_shapes/views.py
+++ MP_WebApp/mining_shapes/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+
 from .forms import RunSessionForm
 from .models import Session
 from .model_utils import put_images_in_vesselmodel
 from .form_utils import get_name_of_choice_field, get_features_from_feature_field
+
+FEATURE_JOIN_STR = '+'
 
 
 def index(request):
@@ -12,12 +19,23 @@ def index(request):
             session = Session(user=request.user)
             session.save()
             put_images_in_vesselmodel(session, request.FILES.getlist('images'))
-            choice = get_name_of_choice_field(form['model'])
+            model_choice = get_name_of_choice_field(form['model'])
             features = get_features_from_feature_field(
                 form.cleaned_data.get("features"))
-            print("HE")
-            # handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect(reverse("process", kwargs={
+                'session_id': session.pk,
+                'seg_model': model_choice,
+                'features': FEATURE_JOIN_STR.join(features)}))
 
     return render(request, "mining_shapes/index.html", {
         "form": RunSessionForm(),
+    })
+
+
+@login_required
+def process(request, session_id: int, seg_model: str, features: str):
+    return render(request, "mining_shapes/process.html", {
+        "test": session_id,
+        "seg": seg_model,
+        "features": features.split(FEATURE_JOIN_STR)
     })
